32d88d96ed9aac8017700e1790405616cb6aeaff
tweeneetee <trinity.ljq@gmail.com>
2024-05-22 19:29:22 +0800

    remove node

--- node_modules/picmo/dist/recents/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { LocalStorageProvider } from './LocalStorageProvider';
-export { SessionStorageProvider } from './SessionStorageProvider';
-export { RecentsProvider } from './RecentsProvider';
-export { InMemoryProvider } from './InMemoryProvider';
